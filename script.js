c6ad145109b3f494a158a727970905f3a51db2e1
maratgmk <142890483+maratgmk@users.noreply.github.com>
2023-10-19 09:18:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+alert(100)
+const number = +prompt("Input number")
+console.log(0.1*number)
+
+const x = +prompt("First number")
+const y = +prompt("Second number")
+if(x<y) {
+    console.log(`The least number is : ${x}`)
+}else{
+    console.log(`The least number is : ${y}`)
+}
+
+
+
+  const a = +prompt("Input any number")
+  if(a<100) {
+      console.log(`Число ${a} меньше 100`)}
+      else  if(a>100){
+          console.log(`Число ${a} больше 100`)
+       }else{
+         
+          console.log(`Число ${a} равняется 100`)}
+      
+
+const yourname = prompt("What is your name?")
+const age = +prompt("How old are you?")
+
+if(age>18){
+console.log(`Hello, ${yourname} `)
+}else{
+    console.log(`Hi, ${yourname}`)
+
+}
+
+    
+    
+
