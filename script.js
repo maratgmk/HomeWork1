652aa5d221e35692671b6370b3cb537fe8eb918d
maratgmk <142890483+maratgmk@users.noreply.github.com>
2023-10-23 22:22:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-alert(100)
-const number = +prompt("Input number")
-console.log(0.1*number)
+alert ("Hello") 
 
-const x = +prompt("First number")
-const y = +prompt("Second number")
-if(x<y) {
-    console.log(`The least number is : ${x}`)
-}else{
-    console.log(`The least number is : ${y}`)
+ for(let i = 0; i<=10; i+=2){
+     console.log(i);    
+ }
+
+ for(let i = 55; i>=20 ; i--){
+     console.log(i)
+ }
+
+const numbers = [3, 5, 11, 2, 8, 1, 6];
+  for(let i = 0; i < numbers.length; i++){
+      console.log(numbers[i])
+  }
+
+console.log(`numbers = [${numbers[0]},${numbers[1]},${numbers[2]},${numbers[3]},${numbers[4]},${numbers[5]},${numbers[6]}]`)
+
+const numbers_sqared = []
+for(let i = 0; i < numbers.length; i ++){
+   numbers_sqared.push(numbers[i]*numbers[i])
 }
+console.log(numbers_sqared)
+console.log(`numbers_sqared = [${numbers_sqared[0]},${numbers_sqared[1]},${numbers_sqared[2]},${numbers_sqared[3]},${numbers_sqared[4]},${numbers_sqared[5]},${numbers_sqared[6]}]`)
 
+const last_elem = numbers_sqared[numbers_sqared.length - 1]
+console.log(`last_elem = ${last_elem}`)
+console.log(`last_elem = ${numbers_sqared[numbers_sqared.length - 1]}`)
 
+const user = {
+    first_name: 'Ivan', 
+    last_name: 'Ivanov', 
+    age: 20, 
+    salary: 500
+    }
+    console.log(`User's name is ${user.first_name} ${user.last_name}. He is ${user.age} yaers old.`)
 
-  const a = +prompt("Input any number")
-  if(a<100) {
-      console.log(`Число ${a} меньше 100`)}
-      else  if(a>100){
-          console.log(`Число ${a} больше 100`)
-       }else{
-         
-          console.log(`Число ${a} равняется 100`)}
-      
-
-const yourname = prompt("What is your name?")
-const age = +prompt("How old are you?")
-
-if(age>18){
-console.log(`Hello, ${yourname} `)
-}else{
-    console.log(`Hi, ${yourname}`)
-
-}
-
-    
-    
 
